cf7cd7ee4c8ea8f19482747d668efb5732b27993
ritesh.modi <rimod@microsoft.com>
2023-11-08 11:11:58 +0000

    new metric type in web-classification

--- web_classification/flows/evaluation_adv/calculate_accuracy.py
+++ web_classification/flows/evaluation_adv/calculate_accuracy.py
@@ -13,5 +13,4 @@ def calculate_accuracy(grades: List[str]):
     # calculate accuracy for each variant
     accuracy = round((result.count("Correct") / len(result)), 2)
     log_metric("precision", accuracy)
-
     return result
