18c8f411d3f600f088f6d0ef3b0e125e3f257b87
ritesh.modi <rimod@microsoft.com>
2023-12-18 09:54:56 +0000

    test new features

--- llmops/common/deployment/test_local_flow.py
+++ llmops/common/deployment/test_local_flow.py
@@ -9,6 +9,7 @@ This argument is required to specify the name of the flow for execution.
 import argparse
 import json
 import requests
+import time
 from llmops.common.logger import llmops_logger
 logger = llmops_logger("test local container endpoint")
 
@@ -31,11 +32,16 @@ url = "http://0.0.0.0:8080/score"
 
 headers = {'Content-Type': 'application/json'}
 
-
-response = requests.post(url, data=json.dumps(json_data), headers=headers)
-if response.status_code == 200:
-    print("POST request successful!")
-    print("Response:", response.json())  # Print the response content
-else:
-    print("POST request failed with status code:", response.status_code)
-    raise Exception("Value cannot be negative")
+max_retries = 3
+retry_delay = 2
+for attempt in range(max_retries):
+    try:
+        response = requests.post(url, data=json.dumps(json_data), headers=headers, timeout=30)
+        if response.status_code == 200:
+            print("POST request successful!")
+            print("Response:", response.json())  # Print the response content
+        else:
+            print("POST request failed with status code:", response.status_code)        
+    except (requests.exceptions.RequestException, ConnectionResetError) as e:
+        print(f"Error occurred: {e}")
+        time.sleep(retry_delay)
