5ca3d68449c1f24c98fac48ac811472281ce9e11
ritesh.modi <rimod@microsoft.com>
2023-12-22 13:49:32 +0000

    added test script for local docker testing

---
+++ llmops/common/deployment/test_local_flow.py
@@ -0,0 +1,47 @@
+"""
+This module executes test on build server for local container endpoint.
+
+Args:
+--flow_to_execute: The name of the flow use case.
+This argument is required to specify the name of the flow for execution.
+"""
+
+import argparse
+import json
+import requests
+import time
+from llmops.common.logger import llmops_logger
+logger = llmops_logger("test local container endpoint")
+
+parser = argparse.ArgumentParser("test local container endpoint")
+
+
+parser.add_argument(
+    "--flow_to_execute", type=str, help="flow to test", required=True
+)
+
+
+args = parser.parse_args()
+
+flow_name = args.flow_to_execute
+
+with open(f"./{flow_name}/sample-request.json", 'r') as file:
+    json_data = json.load(file)
+
+url = "http://0.0.0.0:8080/score"
+
+headers = {'Content-Type': 'application/json'}
+
+max_retries = 3
+retry_delay = 2
+for attempt in range(max_retries):
+    try:
+        response = requests.post(url, data=json.dumps(json_data), headers=headers, timeout=30)
+        if response.status_code == 200:
+            print("POST request successful!")
+            print("Response:", response.json())  # Print the response content
+        else:
+            print("POST request failed with status code:", response.status_code)        
+    except (requests.exceptions.RequestException, ConnectionResetError) as e:
+        print(f"Error occurred: {e}")
+        time.sleep(retry_delay)
