b969ec888f72196d8992a770129055fc9d833d93
Raouf <raoufaliouat@microsoft.com>
2024-01-22 15:00:48 +0000

    disabling private access managed endpoint  when in network isolation

--- infra/bicep/scripts/export-deployment-variables.sh
+++ infra/bicep/scripts/export-deployment-variables.sh
@@ -23,25 +23,24 @@ while getopts "k:g:e:w:r:i:" opt; do
     esac
 done
 
-# The JSON file
-FILE="${repoRoot}/llmops_confdig.json"
+# The LLMOPS configJSON file
+FILE_LLMOPS="${repoRoot}/llmops_config.json"
 
 #If the llmops_config.json file does not exist, create it and add one block containing the environment variables
-if [[ ! -e $FILE ]]; then
-    echo "File $FILE does not exist, creating it..."
-    touch $FILE
+if [[ ! -e $FILE_LLMOPS ]]; then
+    echo "File $FILE_LLMOPS does not exist, creating it..."
+    touch $FILE_LLMOPS
     blockToAdd="{
                     \"ENV_NAME\": \"$envName\",
                     \"RUNTIME_NAME\": \"$runtimeName\",
                     \"KEYVAULT_NAME\": \"$kvName\",
                     \"RESOURCE_GROUP_NAME\": \"$rgName\",
                     \"WORKSPACE_NAME\": \"$workspaceName\",
-                    \"NETWORK_ISOLATION\": $networkIsolationBool,
                     \"STANDARD_FLOW_PATH\": \"flows/experiment\",
                     \"EVALUATION_FLOW_PATH\": \"flows/evaluation, flows/evaluation_adv\"
                 }"
-    echo "File $FILE created."
-    cat << EOF > $FILE
+    echo "File $FILE_LLMOPS created."
+    cat << EOF > $FILE_LLMOPS
 {
     "envs":[
                 $blockToAdd
@@ -51,7 +50,7 @@ EOF
 else
     #If the the llmops_config.json exists, update it. 
     #The update is done by adding a new block if the environment does not exist, or by updating the existing block if the environment already exists
-    echo "File $FILE already exists. Updating it configuration of environment $envName..."
+    echo "File $FILE_LLMOPS already exists. Updating configuration of environment $envName..."
     jq --arg envName "$envName" --arg runtimeName "$runtimeName" --arg kvName "$kvName" --arg rgName "$rgName" --arg workspaceName "$workspaceName" '
     if any(.envs[]; .ENV_NAME == $envName) then
         .envs |= map(
@@ -77,5 +76,69 @@ else
                 "EVALUATION_FLOW_PATH": "flows/evaluation"
             }
         ]
-    end' $FILE > temp.json && mv temp.json $FILE
+    end' $FILE_LLMOPS > temp.json && mv temp.json $FILE_LLMOPS
 fi
+
+# Now set "PUBLIC_ACCESS" to true in deployment_config.json
+FILE_DEPLOYMENT="${repoRoot}/deployment_config.json"
+
+#If the llmops_config.json file does not exist, create it and add one block containing the environment variables
+if [[ ! -e $FILE_DEPLOYMENT ]]; then
+    echo "File $FILE_DEPLOYMENT does not exist, creating it..."
+    touch $FILE_DEPLOYMENT
+    blockToAddDeployment="{
+    \"azure_managed_endpoint\":[
+        {
+            \"ENV_NAME\": \"dev\",
+            \"TEST_FILE_PATH\": \"sample-request.json\",
+            \"PUBLIC_ACCESS\": \"$networkIsolationBool\",
+            \"ENDPOINT_NAME\": \"\",
+            \"ENDPOINT_DESC\": \"An online endpoint serving a flow for [task]\",
+            \"DEPLOYMENT_DESC\": \"prompt flow deployment\",
+            \"PRIOR_DEPLOYMENT_NAME\": \"\",
+            \"PRIOR_DEPLOYMENT_TRAFFIC_ALLOCATION\": \"\",
+            \"CURRENT_DEPLOYMENT_NAME\": \"\",
+            \"CURRENT_DEPLOYMENT_TRAFFIC_ALLOCATION\": \"100\",
+            \"DEPLOYMENT_VM_SIZE\": \"Standard_F4s_v2\",
+            \"DEPLOYMENT_BASE_IMAGE_NAME\": \"mcr.microsoft.com/azureml/promptflow/promptflow-runtime:latest\",
+            \"DEPLOYMENT_CONDA_PATH\": \"environment/conda.yml\",
+            \"DEPLOYMENT_INSTANCE_COUNT\": 1,
+            \"ENVIRONMENT_VARIABLES\": {
+                \"example-name\": \"example-value\"
+            }
+        }
+    ],
+    \"kubernetes_endpoint\":[
+        {
+            \"ENV_NAME\": \"dev\",
+            \"TEST_FILE_PATH\": \"sample-request.json\",
+            \"PUBLIC_ACCESS\": \"$networkIsolationBool\",
+            \"ENDPOINT_NAME\": \"\",
+            \"ENDPOINT_DESC\": \"An kubernetes endpoint serving a flow for [task]\",
+            \"DEPLOYMENT_DESC\": \"prompt flow deployment\",
+            \"PRIOR_DEPLOYMENT_NAME\": \"\",
+            \"PRIOR_DEPLOYMENT_TRAFFIC_ALLOCATION\": \"\",
+            \"CURRENT_DEPLOYMENT_NAME\": \"\",
+            \"CURRENT_DEPLOYMENT_TRAFFIC_ALLOCATION\": 100,
+            \"COMPUTE_NAME\": \"\",
+            \"DEPLOYMENT_VM_SIZE\": \"promptinstancetype\",
+            \"DEPLOYMENT_BASE_IMAGE_NAME\": \"mcr.microsoft.com/azureml/promptflow/promptflow-runtime:latest\",
+            \"DEPLOYMENT_CONDA_PATH\": \"environment/conda.yml\",
+            \"DEPLOYMENT_INSTANCE_COUNT\": 1,
+            \"CPU_ALLOCATION\": \"\",
+            \"MEMORY_ALLOCATION\": \"\",
+            \"ENVIRONMENT_VARIABLES\": {
+                \"example-name\": \"example-value\"
+            }
+        }
+    ]
+}"
+    echo "File $FILE_DEPLOYMENT created."
+    cat << EOF > $FILE_DEPLOYMENT
+    $blockToAddDeployment
+EOF
+else
+    #If the the deployment_config.json.json exists, update it to set value of "PUBLIC_ACCESS" to true
+    jq --arg networkIsolationBool "$networkIsolationBool" '.[] |= (.[] | .PUBLIC_ACCESS = $networkIsolationBool)' $FILE_DEPLOYMENT > tempd.json && mv tempd.json $FILE_DEPLOYMENT
+fi
+
