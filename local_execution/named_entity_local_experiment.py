3f90fca0a679c6d43251b519405cc2bf9a430e34
joaosalvadoMicrosoft <josalvad@microsoft.com>
2024-03-06 11:50:12 +0000

    changed code

--- local_execution/named_entity_local_experiment.py
+++ local_execution/named_entity_local_experiment.py
@@ -4,12 +4,12 @@ load_dotenv()
 from prompt_experimentation.run_local import LocalFlowExecution
 
 def main():
-    data = "named_entity_recognition/data/data.jsonl"
-    flow = "named_entity_recognition/flows/standard"
-    eval_flow = "named_entity_recognition/flows/evaluation"
+    data = "../named_entity_recognition/data/data.jsonl"
+    flow = "../named_entity_recognition/flows/standard"
+    eval_flow = "../named_entity_recognition/flows/evaluation"
     named_entity_flow = LocalFlowExecution(flow, eval_flow, data, {"text": "${data.text}", "entity_type": "${data.entity_type}"})
     named_entity_flow.process_local_flow()
-    named_entity_flow.create_local_connections()
+    #named_entity_flow.create_local_connections()
     run_ids = named_entity_flow.execute_experiment()
 
     named_entity_flow.execute_evaluation(run_ids,data,{
