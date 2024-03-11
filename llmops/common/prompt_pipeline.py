c8c3774716d84010a2657ddcb049a606932a3af9
mohanajuhi166 <mohanab@microsoft.com>
2024-03-11 22:50:39 +0100

    Automatic runtime

--- llmops/common/prompt_pipeline.py
+++ llmops/common/prompt_pipeline.py
@@ -192,13 +192,13 @@ def prepare_and_execute(
                         run = Run(
                             flow=flow,
                             data=data_id,
-                            runtime=runtime,
+                            #runtime=runtime,
                             # un-comment the resources parameter assignment
                             # and update the size of the compute and also
                             # comment the runtime parameter assignment to
                             # enable automatic runtime.
                             # Reference: COMPUTE_RUNTIME
-                            # resources={"instance_type": "Standard_E4ds_v4"},
+                            resources={"instance_type": "Standard_E4s_v3"},
                             variant=variant_string,
                             name=(
                                 f"{experiment_name}_{variant_id}"
