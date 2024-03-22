1431f7f09790c5f828a8ace21279dd9d82f2a9df
mohanajuhi166 <mohanajuhi166@gmail.com>
2024-03-22 09:49:26 +0100

    Prepare Docker Image And Create Webapp Deployment (#10)
    * Docker image and webapp deployment

--- llmops/common/scripts/az_webapp_deploy.sh
+++ llmops/common/scripts/az_webapp_deploy.sh
@@ -46,14 +46,17 @@ az appservice plan create --name $appserviceplan --resource-group $rgname --is-l
 
 # create/update Web App
 az webapp create --resource-group $rgname --plan $appserviceplan --name $appserviceweb --deployment-container-image-name \
-    $REGISTRY_NAME.azurecr.io/"$flow_to_execute"_"$deploy_environment":$build_id
+    $REGISTRY_NAME.azurecr.io/"$flow_to_execute"_"$deploy_environment":"latest"
 
 # create/update Web App config settings
 az webapp config appsettings set --resource-group $rgname --name $appserviceweb \
     --settings WEBSITES_PORT=8080
 
 for name in "${connection_names[@]}"; do
-    api_key=$(echo $CONNECTION_DETAILS | jq -r --arg name "$name" '.[] | select(.name == $name) | .api_key')
+    api_key=$(echo $connection_details | jq -r --arg name "$name" '.[] | select(.name == $name) | .api_key')
+
+     echo "webapp aoai connection"
+     #echo $connection_details
 
     uppercase_name=$(echo "$name" | tr '[:lower:]' '[:upper:]')
     modified_name="${uppercase_name}_API_KEY"
