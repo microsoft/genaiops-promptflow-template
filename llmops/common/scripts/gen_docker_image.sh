a7d3365ffff27fffad4cb8f6fe9779999c02bbfa
mohanajuhi166 <mohanab@microsoft.com>
2024-03-22 11:23:08 +0100

    CD jenkins pipeline test params-check

--- llmops/common/scripts/gen_docker_image.sh
+++ llmops/common/scripts/gen_docker_image.sh
@@ -66,7 +66,7 @@ if [[ -n "$selected_object" ]]; then
 
     REGISTRY_NAME=$(echo "$con_object" | jq -r '.REGISTRY_NAME')
 
-    registry_object=$(echo $REGISTRY_DETAILS | jq -r --arg name "$REGISTRY_NAME" '.[] | select(.registry_name == $name)')
+    registry_object=$(echo $registry_details | jq -r --arg name "$REGISTRY_NAME" '.[] | select(.registry_name == $name)')
     registry_server=$(echo "$registry_object" | jq -r '.registry_server')
     registry_username=$(echo "$registry_object" | jq -r '.registry_username')
     registry_password=$(echo "$registry_object" | jq -r '.registry_password')
