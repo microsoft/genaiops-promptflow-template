752903aa1af4f9e3117897c34974c1d05dcab5dc
mohanajuhi166 <mohanab@microsoft.com>
2024-03-28 13:36:26 +0100

    Fixing default credentials

--- dataops/common/aml_pipeline.py
+++ dataops/common/aml_pipeline.py
@@ -180,12 +180,6 @@ def main():
         help="Root dir for config file",
         required=True,
     )
-    parser.add_argument(
-        "--sa_acc_key",
-        type=str,
-        help="SA Account key for storage account",
-        required=True,
-    )
 
     args = parser.parse_args()
 
