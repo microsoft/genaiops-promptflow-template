ce7e3fb43c6afd5cc3b6f1575ac9b1cdd21c4bac
Raihan Alam <raihanalam@microsoft.com>
2024-04-01 15:00:58 +1100

    prog

--- dataops/common/aml_data_store.py
+++ dataops/common/aml_data_store.py
@@ -32,7 +32,7 @@ def register_data_store(
         description,
         sa_account_name,
         sa_container_name,
-        sa_sas_key,
+        sa_key,
         aml_client
 ):
     store = AzureBlobDatastore(
@@ -40,7 +40,7 @@ def register_data_store(
         description=description,
         account_name=sa_account_name,
         container_name=sa_container_name,
-        credentials= AccountKeyConfiguration(account_key=sa_sas_key)
+        credentials= AccountKeyConfiguration(account_key=sa_key)
     )
     aml_client.create_or_update(store)
 
@@ -66,9 +66,9 @@ def main():
         required=True,
     )
     parser.add_argument(
-        "--sa_sas_key",
+        "--sa_key",
         type=str,
-        help="SAS key for target storage account",
+        help="Storage account key",
         required=True,
     )
     parser.add_argument(
@@ -83,7 +83,7 @@ def main():
     subscription_id = args.subscription_id
     resource_group_name = args.resource_group_name
     workspace_name = args.workspace_name
-    sa_sas_key = args.sa_sas_key
+    sa_key = args.sa_key
     config_path_root_dir = args.config_path_root_dir
 
     config_path = os.path.join(os.getcwd(), f"{config_path_root_dir}/configs/dataops_config.json")
@@ -104,7 +104,7 @@ def main():
         description=config["DATA_STORE_DESCRIPTION"],
         sa_account_name=storage_account,
         sa_container_name=target_container_name,
-        sa_sas_key=sa_sas_key,
+        sa_key=sa_key,
         aml_client=aml_client
     )
 
