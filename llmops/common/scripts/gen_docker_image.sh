f47a886a5cee354c459c068184fe6fc29bda7522
Oleksandr <oteslenkoua@gmail.com>
2024-05-12 19:38:48 +0100

    do not change run file in docker

--- llmops/common/scripts/gen_docker_image.sh
+++ llmops/common/scripts/gen_docker_image.sh
@@ -48,7 +48,7 @@ if [[ -e "$config_path" ]]; then
     pip install -r ./$use_case_base_path/$STANDARD_FLOW/requirements.txt
     pf flow build --source "./$use_case_base_path/$STANDARD_FLOW" --output "./$use_case_base_path/docker"  --format docker 
 
-    cp "./$use_case_base_path/environment/run" "./$use_case_base_path/docker/runit/promptflow-serve/run"
+    # cp "./$use_case_base_path/environment/run" "./$use_case_base_path/docker/runit/promptflow-serve/run"
     cp "./$use_case_base_path/environment/Dockerfile" "./$use_case_base_path/docker/Dockerfile"
 
     # docker build the prompt flow based image
