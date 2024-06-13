eeaeef5a77630d38d7d5267a7ebfb567fecad021
ritesh.modi <rimod@microsoft.com>
2024-06-13 09:07:36 +0100

    updating llmops code for adapting flex flows

--- llmops/common/experiment_cloud_config.py
+++ llmops/common/experiment_cloud_config.py
@@ -1,10 +1,18 @@
+"""
+Configuration for running an experiment in the cloud.
+
+This module provides a class that holds the configuration for running an
+experiment in the cloud. The configuration includes the subscription ID,
+resource group name, and workspace name of the Azure ML workspace.
+"""
+
 import os
 from typing import Optional
 
 
 def _try_get_env_var(variable_name: str) -> str:
     """
-    Try to read environment variable. Raise ValueError if the variable doesn't exist or is empty.
+    Try to read environment variable.
 
     :param variable_name: Environment variable name.
     :type variable_name: str
@@ -15,14 +23,14 @@ def _try_get_env_var(variable_name: str) -> str:
     value = os.environ.get(variable_name)
     if value is None or value == "":
         raise ValueError(
-            f"Environment variable '{variable_name}' is not set or is empty."
+            f"env var '{variable_name}' is not set or is empty."
         )
     return value
 
 
 def _get_optional_env_var(variable_name: str) -> str:
     """
-    Read environment variable. Return None if the variable doesn't exist or is empty.
+    Read environment variable. Return None if it doesn't exist or is empty.
 
     :param variable_name: Environment variable name.
     :type variable_name: str
@@ -55,10 +63,19 @@ class ExperimentCloudConfig:
         env_name: Optional[str] = None,
         compute_target: Optional[str] = None,
     ):
-        self.subscription_id = subscription_id or _try_get_env_var("SUBSCRIPTION_ID")
+        """Initialize the configuration."""
+        self.subscription_id = subscription_id or _try_get_env_var(
+            "SUBSCRIPTION_ID"
+        )
         self.resource_group_name = resource_group_name or _try_get_env_var(
             "RESOURCE_GROUP_NAME"
         )
-        self.workspace_name = workspace_name or _try_get_env_var("WORKSPACE_NAME")
-        self.environment_name = env_name or _get_optional_env_var("ENV_NAME")
-        self.compute_target = compute_target or _get_optional_env_var("COMPUTE_TARGET")
+        self.workspace_name = workspace_name or _try_get_env_var(
+            "WORKSPACE_NAME"
+        )
+        self.environment_name = env_name or _get_optional_env_var(
+            "ENV_NAME"
+        )
+        self.compute_target = compute_target or _get_optional_env_var(
+            "COMPUTE_TARGET"
+        )
