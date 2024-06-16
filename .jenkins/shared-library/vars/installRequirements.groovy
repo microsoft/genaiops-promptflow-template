6caaef4a42d26ebe3496561a4e14017a44da211b
mohanajuhi166 <mohanab@microsoft.com>
2024-06-16 10:35:55 +0200

    Flex flow jenkin fixeS

--- .jenkins/shared-library/vars/installRequirements.groovy
+++ .jenkins/shared-library/vars/installRequirements.groovy
@@ -2,9 +2,8 @@ def call(String requirements_type) {
     withPythonEnv('/usr/bin/python3.9') {
     sh """
     pip install setuptools wheel
-    pip uninstall -y promptflow promptflow-core promptflow-devkit promptflow-azure
     pip install -r .jenkins/requirements/${requirements_type}.txt
-    pip install promptflow-tools promptflow-sdk jinja2 openai promptflow-sdk[builtins]
+    pip install promptflow promptflow-tools promptflow-sdk jinja2 promptflow[azure] openai promptflow-sdk[builtins]
     """
     }
 }
